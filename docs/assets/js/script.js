43b661a78372d08a235371b2bd0c1ab6599f7602
eilennss <cdc.leticiaduarte@gmail.com>
2024-06-24 17:22:42 -0300

    fotocachorro

---
+++ docs/assets/js/script.js
@@ -0,0 +1,6 @@
+function exibirSaudacao() {
+    alert('Olá! Bem-vindo à página de editar dados. Não se esqueça de salvar quando terminar!');
+}
+window.addEventListener('load', function() {
+    exibirSaudacao();
+});
